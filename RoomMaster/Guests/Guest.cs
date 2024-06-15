b8aaad1715779ecff4b7d4b4041cc410b7d35f42
Jean Michael Harris <jeanmichael.harris@us.giti.com>
2024-06-14 22:52:16 -0400

    Save point for UI on HomeWindow.

--- RoomMaster/Guests/Guest.cs
+++ RoomMaster/Guests/Guest.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace RoomMaster.Guests
 {
-    internal class Guest
+    public class Guest
     {
+        public string Name { get; set; }
     }
 }
---
+++ RoomMaster/Rooms/Rooms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoomMaster.Guests;
+
+namespace RoomMaster.Rooms
+{
+    class Rooms
+    {
+        public string RoomNumber { get; set; }
+        public string Status { get; set; }
+        public  Guest guest { get; set; }
+        public DateTime? CheckoutDate { get; set; }
+    }
+}
